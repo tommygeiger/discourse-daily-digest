44e5f1598333f65eaf129ef6808436d7ad0a5818
Thomas Geiger <tommyhawk21@gmail.com>
2022-01-27 01:35:27 -0500

    Update engine.rb

--- lib/discourse_daily_email/engine.rb
+++ lib/discourse_daily_email/engine.rb
@@ -4,56 +4,10 @@ module DiscourseDailyEmail
     config.after_initialize do
       User.register_custom_field_type('user_daily_email_enabled', :boolean)
       require_dependency 'user_notifications'
-      class ::UserNotifications
-        
-        def apply_notification_styles(email)
-                   email.html_part.body = Email::Styles.new(email.html_part.body.to_s).tap do |styles|
-                   styles.format_basic
-                   styles.format_html
-                   end.to_html
-                   email
-        end
-        
-        def mailing_list(user, opts={})
-          prepend_view_path "plugins/discourse-daily-email/app/views"
-
-          @since = opts[:since] || 1.day.ago
-          @since_formatted = short_date(@since)
-
-          topics = Topic
-            .joins(:posts)
-            .includes(:posts)
-            .for_digest(user, 100.years.ago)
-            .where("posts.created_at > ?", @since)
-            .order("posts.id")
-
-          unless user.staff?
-            topics = topics.where("posts.post_type <> ?", Post.types[:whisper])
-          end
-
-          @new_topics = topics.where("topics.created_at > ?", @since).uniq
-          @existing_topics = topics.where("topics.created_at <= ?", @since).uniq
-          @topics = topics.uniq
-
-          return if @topics.empty?
-
-          build_summary_for(user)
-          opts = {
-            from_alias: I18n.t('user_notifications.mailing_list.from', site_name: SiteSetting.title),
-            subject: I18n.t('user_notifications.mailing_list.subject_template', email_prefix: @email_prefix, date: @date),
-            mailing_list_mode: true,
-            add_unsubscribe_link: true,
-            unsubscribe_url: "#{Discourse.base_url}/email/unsubscribe/#{@unsubscribe_key}",
-          }
-
-          apply_notification_styles(build_email(@user.email, opts))
-        end
-      end 
-
       require_dependency 'user_serializer'
+      
       class ::UserSerializer
         attributes :user_daily_email_enabled
-
         def user_daily_email_enabled
           if !object.custom_fields["user_daily_email_enabled"]
             object.custom_fields["user_daily_email_enabled"] = false
@@ -66,10 +20,9 @@ module DiscourseDailyEmail
       module ::Jobs
         class EnqueueDailyEmail < Jobs::Scheduled
           every 1.minute
-
           def execute(args)
             target_user_ids.each do |user_id|
-              Jobs.enqueue(:user_email, type: :daily_email, user_id: user_id)
+              Jobs.enqueue(:user_email, type: UserNotifications::digest, user_id: user_id)
             end
           end
 
@@ -78,11 +31,8 @@ module DiscourseDailyEmail
             User.real
                 .activated
                 .not_suspended
-                .not_silenced
                 .joins(:user_option)
                 .where(id: enabled_ids)
-                .where(staged: false)
-                .where("#{!SiteSetting.must_approve_users?} OR approved OR moderator OR admin")
                 #where subscriber group member
                 .pluck(:id)
           end
