3f28703602ee12588770b1d6fb704045e4238fa3
Thomas Geiger <tommyhawk21@gmail.com>
2022-01-31 01:50:38 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,15 +1,12 @@
 # name: discourse-daily-email
-# about: Daily email for discourse users
-# version: 0.1
+# about: Daily email digest for subscribers
+# version: 1.0
 # author: Tommy Geiger
 # url: https://www.github.com/tommygeiger/discourse-daily-email
 
 enabled_site_setting :daily_email_enabled
-
 DiscoursePluginRegistry.serialized_current_user_fields << "user_daily_email_enabled"
-
 load File.expand_path('../lib/discourse_daily_email/engine.rb', __FILE__)
-
 after_initialize do
   register_editable_user_custom_field :user_daily_email_enabled
 end
