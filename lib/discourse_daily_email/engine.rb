57a8ad715395d561aae005e82971eaf39dcf04b0
Thomas Geiger <tommyhawk21@gmail.com>
2022-01-31 01:39:00 -0500

    Update engine.rb

--- lib/discourse_daily_email/engine.rb
+++ lib/discourse_daily_email/engine.rb
@@ -18,7 +18,7 @@ module DiscourseDailyEmail
 
       module Jobs
         class DailyEmail < ::Jobs::Scheduled
-          every 1.day, at: '5:00 am'
+          every 1.day, at: 500
 
           def execute(args)            
             users.each do |user|
