100ee80f02655e5be2c53f2f8ac16042ee385549
Thomas Geiger <tommyhawk21@gmail.com>
2022-02-18 00:31:23 -0500

    Update engine.rb

--- lib/discourse_daily_email/engine.rb
+++ lib/discourse_daily_email/engine.rb
@@ -3,16 +3,16 @@ module DiscourseDailyEmail
     isolate_namespace DiscourseDailyEmail
     config.after_initialize do
       
-      User.register_custom_field_type('user_daily_email_enabled', :boolean)
+      User.register_custom_field_type('opt_out', :boolean)
       require_dependency 'user_serializer'
       class ::UserSerializer
-        attributes :user_daily_email_enabled
-        def user_daily_email_enabled
-          if !object.custom_fields["user_daily_email_enabled"]
-            object.custom_fields["user_daily_email_enabled"] = true #default true
+        attributes :opt_out
+        def opt_out
+          if !object.custom_fields["opt_out"]
+            object.custom_fields["opt_out"] = false #opt in by default
             object.save
           end
-          object.custom_fields["user_daily_email_enabled"]
+          object.custom_fields["opt_out"]
         end
       end
 
@@ -28,12 +28,12 @@ module DiscourseDailyEmail
           end
 
           def users
-            enabled_ids = UserCustomField.where(name: "user_daily_email_enabled", value: "true").pluck(:user_id)
+            opt_out_ids = UserCustomField.where(name: "opt_out", value: "true").pluck(:user_id)
             User.real
                 .activated
                 .not_suspended
                 .joins(:user_option)
-                .where(id: enabled_ids)
+                .where.not(id: opt_out_ids)
           end
         end
       end
