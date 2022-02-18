ac6607a4a3e6130aa979bafa3f2c2aaa3dce0bce
Thomas Geiger <tommyhawk21@gmail.com>
2022-02-18 00:31:48 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,8 +5,8 @@
 # url: https://www.github.com/tommygeiger/discourse-daily-email
 
 enabled_site_setting :daily_email_enabled
-DiscoursePluginRegistry.serialized_current_user_fields << "user_daily_email_enabled"
+DiscoursePluginRegistry.serialized_current_user_fields << "opt_out"
 load File.expand_path('../lib/discourse_daily_email/engine.rb', __FILE__)
 after_initialize do
-  register_editable_user_custom_field :user_daily_email_enabled
+  register_editable_user_custom_field :opt_out
 end
